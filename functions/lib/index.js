e66810498a9684a5e99e2331d0bd6d53654aee6e
kutumbakasrikruthi <srikruthi.ksk@gmail.com>
2025-07-15 14:44:45 +0530

    new 1

---
+++ functions/lib/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {value: true});
+const firebaseFunctions1 = require("firebase-functions");
+(0, firebaseFunctions1.setGlobalOptions)({maxInstances: 10});
